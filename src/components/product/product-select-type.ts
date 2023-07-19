c44d7015246c7d5373f0e2d2234057aaa2277532
root <root@vps.server.local>
2023-07-19 12:42:50 +0000

    First commit

---
+++ src/components/product/product-select-type.ts
@@ -0,0 +1,14 @@
+export const typeproductlist = [
+  {
+    value: 'fixed_price',
+    label: 'Fixed Price',
+  },
+  {
+    value: 'price_range',
+    label: 'Price Range',
+  },
+  {
+    value: 'price_quotation',
+    label: 'Price Quotation',
+  },
+];
